3e6b32ec78358b6af2a2e7dc257264da5974ac2b
upimariani <upimariani.uniku17@gmail.com>
2023-05-22 05:59:38 +0700

    add pimpinan

--- application/views/Admin/Layouts/sidebar.php
+++ application/views/Admin/Layouts/sidebar.php
@@ -21,8 +21,7 @@
 						<div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
 					</div>
 					<div class="ms-3">
-						<h6 class="mb-0">Jhon Doe</h6>
-						<span>Admin</span>
+						<h6 class="mb-0">Admin</h6>
 					</div>
 				</div>
 				<div class="navbar-nav w-100">
--- metode_abc.sql
+++ metode_abc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 12 Bulan Mei 2023 pada 17.27
+-- Waktu pembuatan: 22 Bulan Mei 2023 pada 00.58
 -- Versi server: 10.4.6-MariaDB
 -- Versi PHP: 7.3.9
 
